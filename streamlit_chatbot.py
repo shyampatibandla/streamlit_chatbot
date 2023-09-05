5640a78c1bf9efce9b8e78908c8ede1b6e2b4d69
shyampatibandla <144091148+shyampatibandla@users.noreply.github.com>
2023-09-05 10:57:40 +0530

    streamlit chatbot code

---
+++ streamlit_chatbot.py
@@ -0,0 +1,44 @@
+import openai
+import streamlit as st
+
+# Set OpenAI API key reading from a file and set default model
+from openai_key import OPENAI_API_KEY
+openai.api_key = OPENAI_API_KEY
+openai_model = "gpt-3.5-turbo"
+
+st.title("OpenAI Chatbot")
+
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+# Accept user input
+if prompt := st.chat_input("What is up?"):
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    
+    # Display user message in chat message container
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    # Display assistant response in chat message container
+    with st.chat_message("assistant"):
+        message_placeholder = st.empty()
+        full_response = ""
+        for response in openai.ChatCompletion.create(
+            model=openai_model,
+            messages=[
+                {"role": m["role"], "content": m["content"]}
+                for m in st.session_state.messages
+            ],
+            stream=True,
+        ):
+            full_response += response.choices[0].delta.get("content", "")
+            message_placeholder.markdown(full_response + "▌")
+        message_placeholder.markdown(full_response)
+    st.session_state.messages.append({"role": "assistant", "content": full_response})
